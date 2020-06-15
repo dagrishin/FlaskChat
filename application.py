0f0ce937fe6d9c4c8cff2c68b0dd907ce8b96dd6
dagrishin <grishin-dim@yandex.ru>
2020-06-15 16:24:17 +0500

    Add flask-wtf for registaration page

--- application.py
+++ application.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
+from wtform_fields import *
+
+
 
 app = Flask(__name__)
 app.secret_key = 'aaaaaaaaaaaaaaa'
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Great success!"
 
-    return render_template("index.html")
+    return render_template("index.html", form=reg_form)
 
 if __name__ == "__main__":
     app.run(debug=True)
